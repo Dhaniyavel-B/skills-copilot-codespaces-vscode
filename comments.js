afd9f5d08af3fd41dc1d6e80f347508cfda55180
unknown <rtrdanny160@gmail.com>
2024-06-19 07:54:43 +0530

    copilot third commit

--- comments.js
+++ comments.js
@@ -1,11 +1,6 @@
-//create web server
+//create a webserver
 const http = require('http');
-const port = 3000;
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World\n');
+  res.end('Hello, world!');
 });
-server.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}/`);
-});
+server.listen(3000);
