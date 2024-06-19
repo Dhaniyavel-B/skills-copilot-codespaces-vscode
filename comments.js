b2008dd966e612ecd4439697511e5e8e962547a9
unknown <rtrdanny160@gmail.com>
2024-06-19 07:22:46 +0530

    copilot third commit

---
+++ comments.js
@@ -0,0 +1,11 @@
+//create web server
+const http = require('http');
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write('<h1>Welcome to Skills Copilot</h1>');
+  res.end();
+});
+server.listen(3000, () => {
+  console.log('Server running at port 3000');
+});
+console.log('Hello World!');
