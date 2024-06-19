e8fa00dabadc60b63cdaf781ccae7caedcb3cbc9
unknown <rtrdanny160@gmail.com>
2024-06-19 08:00:37 +0530

    copilot third commit

--- comments.js
+++ comments.js
@@ -1,6 +1,17 @@
 //create a webserver
-const http = require('http');
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
 const server = http.createServer((req, res) => {
-  res.end('Hello, world!');
-});
-server.listen(3000);
+  if (req.url === "/") {
+    fs.readFile(path.join(__dirname, "index.html"), (err, data) => {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(data);
+      res.end();
+    });
+  }
+}
+server.listen(3000, () => {
+  console.log("Server is running on port 3000");
+}); 
